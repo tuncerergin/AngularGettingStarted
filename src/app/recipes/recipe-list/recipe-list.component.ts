3651431ce4389767b3e732b8f31400c81d920309
Tuncer <ergin.tuncer@hotmal.com>
2020-07-12 00:56:42 +0300

    E03L11 -> "Outputting a List of Recipes with ngFor"

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipies: Recipe[] = [
+  recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test recipe', 'https://diethood.com/wp-content/uploads/2019/07/Whole-Roast-Chicken.jpg')
   ];
 
