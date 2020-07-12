cba6afc8d57514d652ebe899f870800f306470a4
Tuncer <ergin.tuncer@hotmal.com>
2020-07-13 02:32:13 +0300

    E04L06 -> "Binding to Custom Events"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,24 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularGettingStarted';
-  serverElements = [{type: 'server', name: 'Test Server', content: 'Just a test'}];
+  serverElements = [
+    {type: 'server', name: 'Test Server', content: 'Just a test'},
+    {type: 'blueprint', name: 'Test Server', content: 'Just a test'}
+    ];
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(bluePrintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
