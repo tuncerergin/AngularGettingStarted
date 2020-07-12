bc4531fc015d954852f7200195c3dd5f513ae09c
Tuncer <ergin.tuncer@hotmal.com>
2020-07-13 02:07:51 +0300

    E04L04 -> "Binding to Custom Properties"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularGettingStarted';
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'Test Server', content: 'Just a test'}];
 }
