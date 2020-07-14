f94f4ea0f9e538a2604414e4bad0887f0996f381
Tuncer <ergin.tuncer@hotmal.com>
2020-07-14 22:06:23 +0300

    E04L13 -> "Getting Access to the Template & DOM with @ViewChild"

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,7 +8,9 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() {
   }
@@ -17,10 +19,16 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(serverNameInput: HTMLInputElement) {
-    this.serverCreated.emit({serverName: serverNameInput.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({
+      serverName: serverNameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
   }
 
   onAddBlueprint(serverNameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: serverNameInput.value, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({
+      serverName: serverNameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
   }
 }
