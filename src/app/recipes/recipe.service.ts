7f377f38f4cdf7d86f44cbfdcc2640ae257141ba
Tuncer <ergin.tuncer@hotmal.com>
2020-07-19 01:08:54 +0300

    E10L07 -> "Adding Ingredients to Recipes"

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,23 @@ import {Recipe} from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test recipe', 'https://diethood.com/wp-content/uploads/2019/07/Whole-Roast-Chicken.jpg'),
-    new Recipe('A Test Recipe 2', 'This is a test recipe 2', 'https://diethood.com/wp-content/uploads/2019/07/Whole-Roast-Chicken.jpg')
+    new Recipe(
+      'Tasty Schinitzel',
+      'A super-tasty Schinitzel - just awesome',
+      'https://cdn.yemek.com/mncrop/600/315/uploads/2014/11/sinitzel-yeni.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Frenc Fries', 20)
+      ]),
+
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://i2.milimaj.com/i/milliyet/75/0x410/5c8d9e3245d2a04bdc3f8ea2.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
 
   ];
 
